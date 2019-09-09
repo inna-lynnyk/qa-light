30dfe47ad3b4bd8d5855301d5a9d9351bbd963f0
Inna Lynnyk <inna.lynnyk@globallogic.com>
2019-09-09 16:52:43 +0300

    Changed 3 chapter 4 task

--- src/Lesson3/Task4.java
+++ src/Lesson3/Task4.java
@@ -1,8 +1,24 @@
 package Lesson3;
 
 public class Task4 {
-/*  hq [1] - id = 1;
-    hq [3] - id = 2;
-    hq [4] - id = 0;
- */
+int id = 0;
+public static void main(String [] args) {
+    int x = 0;
+    Task4 [] hq = new Task4[5];
+    while (x > 3) {
+        hq[x] = new Task4();
+        hq[x].id = x;
+        x = x + 1;
+    }
+    hq[3] = hq[1];
+    hq[4] = hq[1];
+    hq[3] = null;
+    hq[4] = hq[0];
+    hq[0] = hq[3];
+    hq[3] = hq[2];
+    hq[2] = hq[0];
+    // hq[1] id = 1;
+    // hq[3] id = 2;
+    // hq[4] id = 0;
+    }
 }
